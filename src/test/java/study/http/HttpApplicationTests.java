d3ae48acd14fea50528cc158af917ab5f0a62007
jaejoon <dkansk924@naver.com>
2021-04-04 19:29:12 +0900

    project create

---
+++ src/test/java/study/http/HttpApplicationTests.java
@@ -0,0 +1,13 @@
+package study.http;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
